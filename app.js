b04ecb17969b6b7aebda23ad6f2fc6c28997ce21
ImAadarsh <aadarshkavita@gmail.com>
2023-07-16 17:30:03 +0530

    Details

--- app.js
+++ app.js
@@ -58,7 +58,55 @@ app.post('/places', async (req, res, next) => {
       res.status(500).json({ error: 'Something went wrong' });
     }
   });
+
+  app.post('/get-place-details', async (req, res) => {
+    try {
+      const placeId = req.body.placeId; // Assuming the place ID is sent in the request body
   
+      // Construct the URL with the place ID and Google Maps API key
+      const googleMapsApiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${encodeURIComponent(
+        placeId
+      )}&key=${process.env.GOOGLE_MAP_API}`;
+  
+      // Make the HTTP request to the Google Places Details API
+      const response = await axios.get(googleMapsApiUrl);
+  
+      // Parse the response to extract the address components
+      const result = response.data.result;
+      const addressComponents = result.address_components || [];
+      let streetAddress = '',
+        city = '',
+        state = '',
+        country = '',
+        zip = '';
+  
+      for (const component of addressComponents) {
+        if (component.types.includes('street_number') || component.types.includes('route')) {
+          streetAddress += component.long_name + ' ';
+        } else if (component.types.includes('locality')) {
+          city = component.long_name;
+        } else if (component.types.includes('administrative_area_level_1')) {
+          state = component.long_name;
+        } else if (component.types.includes('country')) {
+          country = component.long_name;
+        } else if (component.types.includes('postal_code')) {
+          zip = component.long_name;
+        }
+      }
+  
+      // Return the address details as the response
+      res.json({
+        streetAddress: streetAddress.trim(),
+        city,
+        state,
+        country,
+        zip,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  });
 
 // No Route Error Handler
 app.use((req, res, next) => {
